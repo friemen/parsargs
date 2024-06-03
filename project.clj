6033b29553633797c8b63cf52fb60482fe60a7ee
Falko Riemenschneider <riemenschneider@doctronic.de>
2024-06-03 08:11:30 +0200

    Use Clojure 1.10.x

--- project.clj
+++ project.clj
@@ -1,16 +1,31 @@
-(defproject parsargs "1.2.0"
-  :description "Parsing concise argument sequences to nice data structures."
-  :url "https://github.com/friemen/parsargs"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :plugins [[codox "0.8.10"]]
-  :codox {:defaults {}
-          :sources ["src"]
-          :exclude []
-          :src-dir-uri "https://github.com/friemen/parsargs/blob/master/"
-          :src-linenum-anchor-prefix "L"}
-  :scm {:name "git"
-        :url "https://github.com/friemen/parsargs"}
-  :repositories [["clojars" {:url "https://clojars.org/repo"
-                             :creds :gpg}]])
+(defproject parsargs "1.2.1-SNAPSHOT"
+  :description
+  "Parsing concise argument sequences to nice data structures."
+
+  :url
+  "https://github.com/friemen/parsargs"
+
+  :license
+  {:name "Eclipse Public License"
+   :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies
+  [[org.clojure/clojure "1.10.3"]]
+
+  :plugins
+  [[lein-codox "0.10.8"]]
+
+  :codox
+  {:defaults {}
+   :sources ["src"]
+   :exclude []
+   :src-dir-uri "https://github.com/friemen/parsargs/blob/master/"
+   :src-linenum-anchor-prefix "L"}
+
+  :scm
+  {:name "git"
+   :url "https://github.com/friemen/parsargs"}
+
+  :repositories
+  [["clojars" {:url "https://clojars.org/repo"
+               :creds :gpg}]])
